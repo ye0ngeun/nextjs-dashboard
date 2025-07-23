d9dbc8b797139ec1bb6fdb2db0e3bdd0276a639b
ye0ngeun <duddms2415@naver.com>
2025-07-23 11:54:43 +0900

    Chapter 10 - Partail Prerendering

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    ppr: 'incremental' // Adopt PPR for specific routes
+  }
 };
 
 export default nextConfig;
